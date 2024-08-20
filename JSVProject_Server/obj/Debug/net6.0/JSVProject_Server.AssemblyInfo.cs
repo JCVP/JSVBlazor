556d9386b138df5b9193a23b3c4dea8ba95daff6
JCVP <jcruz.vp@outlook.com>
2024-08-19 21:30:06 -0300

    ValDaños
    se soluciono localidades y pagina inicial

--- JSVProject_Server/obj/Debug/net6.0/JSVProject_Server.AssemblyInfo.cs
+++ JSVProject_Server/obj/Debug/net6.0/JSVProject_Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JSVProject_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ca216ebb48e8193ae3caa84265a15c6c7c59fb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("JSVProject_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JSVProject_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
